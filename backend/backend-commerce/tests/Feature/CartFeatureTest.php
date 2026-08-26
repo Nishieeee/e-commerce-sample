e25b0f9d1e8c857482e7b9dbf20a41bfcddea758
Nishieeee <pagaroganjhonclein@gmail.com>
2026-08-26 20:28:14 +0800

    feat(test): cartfeature test addtocart endpoint

--- backend/backend-commerce/tests/Feature/CartFeatureTest.php
+++ backend/backend-commerce/tests/Feature/CartFeatureTest.php
@@ -1,7 +1,38 @@
 <?php
 
-test('example', function () {
-    $response = $this->get('/');
+namespace Tests\Feature;
 
-    $response->assertStatus(200);
-});
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+
+use App\Models\CartItem;
+use App\Models\Cart;
+use App\Models\Product;
+use App\Models\User;
+
+class CartFeatureTest extends TestCase
+{
+    use RefreshDatabase;
+    use WithFaker;
+
+    public function test_add_to_cart() {
+        $user = User::factory()->create();
+        $product = Product::factory()->create();
+
+        
+        $response = $this
+        ->actingAs($user)
+        ->postJson('/api/cart/items', [
+           'product_id'=>$product->id,
+           'quantity'=>2,
+        ]);
+
+        $response->assertStatus(200);
+
+        $this->assertDatabaseHas('cart_items', [
+           'product_id' => $product->id,
+           'quantity' => 2, 
+        ]);
+    }
+}
